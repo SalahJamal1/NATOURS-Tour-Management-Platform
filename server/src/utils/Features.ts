dbd15d17bf9cdeadc811a385fd9fa2ea16273765
Salah <salah@Salahs-MacBook-Air.local>
2025-10-14 07:51:44 +0300

    first commit

--- server/src/utils/Features.ts
+++ server/src/utils/Features.ts
@@ -8,7 +8,7 @@ import { CorsOptions } from 'cors';
 import { Options } from 'express-rate-limit';
 dotenv.config();
 
-const DB_URI: string = process.env.DB_URI;
+const DB_URI: string = process.env.MONGO_URI!;
 
 export class Features {
   limitOptions: Partial<Options> = {
